32bd72e775bf798d1aed6bab2e24668efd97fd13
Priyanka Lad <priyanka.i.lad@gmail.com>
2021-04-06 13:51:36 +0530

    partially done desktop view

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+import rulesImg from "../images/image-rules.svg";
+import closeButton from "../images/icon-close.svg";
+import { hidden } from "kleur";
 
 export default function Footer() {
+  const [modal, setModal] = useState(false);
+
+  function closeModal() {
+    setModal(false);
+  }
+
+  function openModal() {
+    setModal(true);
+  }
+
   return (
     <footer>
-      <a className="linkRules" href="#">
+      <button className="btnRules" onClick={openModal}>
         Rules
-      </a>
+      </button>
+      <div
+        id="rules-modal"
+        style={modal ? { visibility: "visible" } : { visibility: "hidden" }}
+        className="modalBox"
+      >
+        <div className="rules-container">
+          <h1 className="heading">rules</h1>
+          <img className="img-rules" src={rulesImg} alt="game rules" />
+          <button class="btnClose" onClick={closeModal}>
+            <img src={closeButton} alt="game rules" />
+          </button>
+        </div>
+      </div>
     </footer>
   );
 }
